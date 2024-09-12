8659516fe8927b21f3d086312421fd19a5451f6a
Rodrigo René Cura <rodrigo.renecura@gmail.com>
2024-09-12 14:27:42 -0300

    Replaza herencia por composición en flee

--- scripts/behaviors/flee.gd
+++ scripts/behaviors/flee.gd
@@ -1,5 +1,7 @@
-extends SeekBehavior
+extends Behavior
 class_name FleeBehavior
 
+var seek := SeekBehavior.new()
+
 func steer(agent: CharacterBody3D) -> Vector3:
-	return super.steer(agent) * -1
+	return seek.steer(agent) * -1
