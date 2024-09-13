9e5da2bcd8eb3c25c639c806d81ec13662662730
Rodrigo <arielsebastianandres@gmail.com>
2024-09-13 10:21:33 -0300

    Creada clase para hacer querys espaciales dinámicas para los steering que dependen de la vecindad cercana

--- scripts/behaviors/separation.gd
+++ scripts/behaviors/separation.gd
@@ -3,26 +3,11 @@ class_name SeparationBehavior
 
 @export var radius = 10.0
 
-var shape_rid
-
-func _ready() -> void:
-	shape_rid = PhysicsServer3D.sphere_shape_create()
-	PhysicsServer3D.shape_set_data(shape_rid, radius)
+@onready var query := NeighborsQuery.new(radius)
 
 func steer(agent: CharacterBody3D) -> Vector3:
-	var query := PhysicsShapeQueryParameters3D.new()
-	var direct_space_state = agent.get_world_3d().direct_space_state
-	query.shape_rid = shape_rid
-	#query.collide_with_areas = true
-	query.collision_mask = agent.collision_mask
-	query.transform = agent.global_transform
-	var result := direct_space_state.intersect_shape(query)
-	
-	var otros = []
-	for c in result:
-		if c["collider"] != agent:
-			otros.append(c["collider"])
+	var otros = query.get_neighbors(agent)
 	
 	
-	print(otros)
+	
 	return Vector3.ZERO
---
+++ scripts/neighbors_query.gd
@@ -0,0 +1,20 @@
+extends Object
+class_name NeighborsQuery
+
+var query: PhysicsShapeQueryParameters3D
+
+func _init(radius: float = 2.0):
+	var shape_rid = PhysicsServer3D.sphere_shape_create()
+	PhysicsServer3D.shape_set_data(shape_rid, radius)
+	
+	query = PhysicsShapeQueryParameters3D.new()
+	query.shape_rid = shape_rid
+
+func get_neighbors(agent: CharacterBody3D) -> Array:
+	query.collision_mask = agent.collision_mask
+	query.transform = agent.global_transform
+	 
+	var direct_space_state = agent.get_world_3d().direct_space_state
+	var result := direct_space_state.intersect_shape(query)
+	
+	return result.filter(func(c): return c["collider"] != agent).map(func(c): return c["collider"])
